4557accd28beed00e2bd2f6d5bacb55fcef8c688
NataliCastillo <natalicastillo1508@gmail.com>
2023-12-02 10:26:57 -0500

    mi primer commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Hola mundo")
+console.log("Mi nombre es Natali")
+console.log("Adios")
+
+var mititulo = "Mi primera pagina";
+console.log(mititulo)
+
+var mietiquetatitulo = document.getElementById("title");
+
+console.log(mietiquetatitulo.firstChild);
+
+var listaDeEtiquetas = document.getElementsByTagName("a");
+
+console.log(listaDeEtiquetas);
+
+console.log(listaDeEtiquetas[2])
+
+var pepinillo = document.getElementsByClass("item");
+console.log(pepinillo);
+console.log(pepinillo[3]);
